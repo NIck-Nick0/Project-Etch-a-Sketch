a27bf49db53d915d6d05cb6c2c908fe329470e1a
NIck-Nick0 <islamomar54444@gmail.com>
2026-02-13 07:42:59 +0200

    add def sizes

--- project.js
+++ project.js
@@ -5,41 +5,48 @@ const parentDiv = document.getElementById('parentDiv');
 const myDiv = document.createElement("div");
 document.body.append(myDiv) ;
 
-myDiv.textContent = "Red";
-myDiv.classList.add("red");
-const red = document.querySelector(".red");
-myDiv.style.color = "red" ;
+let myNumber ;
+
+function Grid() {
+    let myNumber = prompt("Please enter number of boxes per side (e.g., 10):");
+    
+    if (myNumber !== null && myNumber !== "") {
+        let num = parseInt(myNumber);
+        
+     
+        let size = 500 / num;
+        
+        draw(num, size);
+    }
+}
 
 
 
-
-
-
-for (let i = 0; i < 100; i++) {
+function draw (elm , px){
+  parentDiv.innerHTML = '';
+for (let i = 0; i < elm*elm; i++) {
   const myDiv = document.createElement("div");
 
   myDiv.classList.add("red");
   
-  // التنسيق
   myDiv.style.border = "1px solid black";
-  myDiv.style.width = "48px";
-  myDiv.style.height = "48px";
+  myDiv.style.width = px+"px";
+  myDiv.style.height = px+"px";
 
     myDiv.style.margin = "0"
   myDiv.style.padding = "0"
 
-  // الآن يتم إضافة نسخة جديدة في كل مرة
   parentDiv.appendChild(myDiv);
 
-
-
-
-
-  myDiv.addEventListener('mouseenter', function() {
+  myDiv.style.boxSizing = "border-box";
+   myDiv.addEventListener('mouseenter', function() {
   this.classList.add('active-hover');
-    myDiv.style.opacity += "10"
 
 });
+
+}
+
+ 
 }
 
 
